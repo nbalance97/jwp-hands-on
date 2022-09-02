52e4c675e15f4cca72bcff3a6fa97bc17c4afecb
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-02 17:22:42 +0900

    feat: 0~3단계 미션 진행

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,25 @@
 package com.example.etag;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import com.example.version.CacheBustingWebConfig;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter = new FilterRegistrationBean<>(
+                new ShallowEtagHeaderFilter());
+
+        shallowEtagHeaderFilter.addUrlPatterns(
+                "/etag",
+                "/etag/*",
+                "/resources/*"
+        );
+        return shallowEtagHeaderFilter;
+    }
 }
