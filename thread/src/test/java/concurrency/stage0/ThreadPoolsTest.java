ca3ce42fd562fa976a10cddbfc02392b93256d44
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-09-06 12:00:41 +0900

    feat: stage0 테스트 통과

--- thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
+++ thread/src/test/java/concurrency/stage0/ThreadPoolsTest.java
@@ -31,8 +31,12 @@ class ThreadPoolsTest {
         executor.submit(logWithSleep("hello fixed thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
-        final int expectedQueueSize = 0;
+        final int expectedPoolSize = 2;
+        final int expectedQueueSize = 1;
+        /*
+        왜냐하면 초기 newFixedThreadPool 메서드를 호출할때 최대 쓰레드 생성 개수를 2개로 제한해 두었다.
+        따라서 logWithSleep 두개는 쓰레드로 들어가게 되고, 나머지 하나의 logWithSleep은 큐에서 대기하게 되기 때문이다.
+        */
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
         assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
@@ -46,8 +50,13 @@ class ThreadPoolsTest {
         executor.submit(logWithSleep("hello cached thread pools"));
 
         // 올바른 값으로 바꿔서 테스트를 통과시키자.
-        final int expectedPoolSize = 0;
+        final int expectedPoolSize = 3;
         final int expectedQueueSize = 0;
+
+        /*
+        왜냐하면 초기 newFixedThreadPool 메서드를 호출할때 최대 쓰레드 생성 개수를 제한해 두지 않았기 때문에 계속해서 생성한다.
+        따라서 logWithSleep 두개는 쓰레드로 들어가게 되고, 나머지 하나의 logWithSleep은 큐에서 대기하게 되기 때문이다.
+        */
 
         assertThat(expectedPoolSize).isEqualTo(executor.getPoolSize());
         assertThat(expectedQueueSize).isEqualTo(executor.getQueue().size());
