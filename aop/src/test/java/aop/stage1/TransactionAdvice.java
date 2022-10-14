b50e8fba2fcfd3d11a4f9435574dd8694281366f
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-14 20:38:57 +0900

    feat: stage1 문제 해결 완료

--- aop/src/test/java/aop/stage1/TransactionAdvice.java
+++ aop/src/test/java/aop/stage1/TransactionAdvice.java
@@ -2,14 +2,34 @@ package aop.stage1;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import aop.DataAccessException;
 
 /**
  * 어드바이스(advice). 부가기능을 담고 있는 클래스
  */
 public class TransactionAdvice  implements MethodInterceptor {
 
+    private final PlatformTransactionManager transactionManager;
+
+    public TransactionAdvice(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
     @Override
     public Object invoke(final MethodInvocation invocation) throws Throwable {
-        return null;
+        TransactionStatus transaction = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object methodReturnValue = invocation.proceed();
+            transactionManager.commit(transaction);
+            return methodReturnValue;
+        } catch (Exception e) {
+            transactionManager.rollback(transaction);
+            throw new DataAccessException();
+        }
     }
 }
